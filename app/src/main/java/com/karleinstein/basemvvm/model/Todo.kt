3d57af97a50723bd52e55fb3b700b03e9bab6939
tuannq-0847 <karl@karls-MacBook-Pro.local>
2026-02-11 21:56:31 +0700

    init

---
+++ app/src/main/java/com/karleinstein/basemvvm/model/Todo.kt
@@ -0,0 +1,9 @@
+package com.karleinstein.basemvvm.model
+
+import java.io.Serializable
+
+data class Todo(
+    val id: Long,
+    val title: String,
+    val isDone: Boolean = false
+) : Serializable
